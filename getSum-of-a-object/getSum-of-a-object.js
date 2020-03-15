3f0f197b7679caf11e4a910367e08513611bfd1a
leon-99 <leonzifer@gmail.com>
2020-03-15 11:14:21 +0630

    added getSum of an Object

---
+++ getSum-of-a-object/getSum-of-a-object.js
@@ -0,0 +1,17 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+function getSum(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        sum += obj[key];
+    }
+    return sum
+}
+
+let sum = getSum(salaries);
+
+console.log(sum);
