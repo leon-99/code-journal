6ca5a5f2b4890f1aaa347d06dd9ca54db2d75511
leon-99 <leonzifer@gmail.com>
2020-03-16 16:56:38 +0630

    changes

--- JavaScript/google-map-api-with-multiple-markers/google-map-api-wirh-multiple-markers.js
+++ JavaScript/google-map-api-with-multiple-markers/google-map-api-wirh-multiple-markers.js
@@ -4,8 +4,11 @@ function initMap() {
     // Setting Center of the Map
     var center = { lat: 21.983146, lng: 96.08444 };
 
+    // Selcting the div to show the map
+    let mapDiv = document.querySelector('map-div');
+
     // Showing the map with center coordinates
-    var map = new google.maps.Map(document.getElementById("multi-marker-map"), {
+    var map = new google.maps.Map(mapDiv, {
         zoom: 5,
         center: center
     });
