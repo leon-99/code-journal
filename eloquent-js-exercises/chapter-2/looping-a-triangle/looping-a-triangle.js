7ff65142fd623041ff47026f3e72e840ab08a297
leon-99 <leonzifer@gmail.com>
2020-06-07 18:16:50 +0630

    changes: fixed looping a triangle with better code

--- eloquent-js-exercises/chapter-2/looping-a-triangle/looping-a-triangle.js
+++ eloquent-js-exercises/chapter-2/looping-a-triangle/looping-a-triangle.js
@@ -1,12 +1,1 @@
-for (let i = 0; i < 7; i++) {
-    for (let j = 0; j < i; j++) {
-        document.write('#')
-    }
-    document.write('<br>')
-}
-
-/*
- j represents colunm
- i represents rows
- each colunm's hash count is as same as the index of the row
-*/
+for (let i = '*'; i.length <= 7; i+= '*') console.log(i)
