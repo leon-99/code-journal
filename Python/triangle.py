ad21db08f2d7cb20dcdeb9705ece8a1aa77fa048
leon-99 <leonzifer@gmail.com>
2020-08-10 15:46:45 +0630

    removed maps

--- Python/triangle.py
+++ Python/triangle.py
@@ -1,4 +1,8 @@
-for i in range(6):
-    for j in range(i):
-        print("*", end="")
-    print("*")
+def loop_triangle(n):
+    for i in range(1, n):
+        for j in range(i):
+            print("*", end=" ")
+        print('\n')
+
+loop_triangle(int(input('How many stars you wanna print?: ')))
+loop_triangle(int(input('How many stars you wanna print?: ')))
