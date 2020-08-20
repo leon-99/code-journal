c212e58d635d2b82b1b9e2444afd450b0e4fe476
leon-99 <leonzifer@gmail.com>
2020-08-20 22:13:16 +0630

    Aug 20

--- JavaScript/array-methods/arr.Foreach.js
+++ JavaScript/array-methods/arr.Foreach.js
@@ -20,7 +20,6 @@ let arr = ['Leon', 'Klaus', 'Katheine', 'Diana'];
 
 arr.forEach((item, index, array) => {
     console.log(`${item} is at index ${index}.`);
-    console.log(`All items in the array: ${array}`);
 })
 
 // index arg and arr arg are optional. don't include them if you don't need them.
