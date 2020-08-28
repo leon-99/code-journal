9ac6360e9606e74efc6f5ac4881372340f6216b1
leon-99 <leonzifer@gmail.com>
2020-08-28 17:05:15 +0630

    deleted python

--- Python/module.py
+++
@@ -1,6 +0,0 @@
-#   A module can be stand-alone runnable script.
-def hello():
-    print('Hello World')
-if __name__ == "__main__":
-    import helloworld
-    helloworld.hello()
--- Python/triangle.py
+++
@@ -1,8 +0,0 @@
-def loop_triangle(n):
-    for i in range(1, n):
-        for j in range(i):
-            print("*", end=" ")
-        print('\n')
-
-loop_triangle(int(input('How many stars you wanna print?: ')))
-loop_triangle(int(input('How many stars you wanna print?: ')))
